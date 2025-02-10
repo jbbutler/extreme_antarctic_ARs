075206c0f68aa4eb9c615d698e0f1b886917abf4
James Butler <jbbutler@login36.chn.perlmutter.nersc.gov>
2025-02-09 22:10:35 -0800

    edits to sensitivity_test file

--- scripts/.ipynb_checkpoints/sensitivity_tests-checkpoint.py
+++ scripts/.ipynb_checkpoints/sensitivity_tests-checkpoint.py
@@ -21,7 +21,7 @@ from utils import is_landfalling
 import utils
 
 ######## SETUP ########
-scratch_dir = '/scratch/users/butlerj/extreme_antarctic_ars/'
+scratch_dir = '/pscratch/sd/j/jbbutler/extreme_antarctic_ars/'
 catalog_subset = utils.load_catalogs()
 ais_pts = utils.load_ais()
 
@@ -32,7 +32,7 @@ baseline_par_dict = {'seed':12345,
                      'rep_pts': 10,
                      'min_pts': 5}
 
-par_perturbations_dict = {'eps_time': [12, 14, 16, 18, 20, 22, 24],
+par_perturbations_dict = {'eps_time': [14, 16, 18, 20, 22, 24],
                          'rep_pts': [15, 20, 50]}
 
 # make the combos of parameters
@@ -62,6 +62,7 @@ for key in combos.keys():
     par_dicts = combos[key]
     
     for par_dict in par_dicts:
+        print('starting a new set')
         
         if key == 'baseline':
             par_dir = key_dir
@@ -86,5 +87,6 @@ for key in combos.keys():
         
         dataframe = utils.construct_dataframe(obj_subset, ais_pts)
         dataframe.to_hdf(par_dir + '/storm_df.h5', key='df')
-        da = utils.construct_dataarray(coord_dict=catalog_subset, big_df=obj_subset)
+        coord_dict = {'lats': catalog_subset.lat, 'lons': catalog_subset.lon}
+        da = utils.construct_dataarray(coord_dict=coord_dict, big_df=obj_subset)
         da.to_netcdf(par_dir + '/storm_da.nc')
