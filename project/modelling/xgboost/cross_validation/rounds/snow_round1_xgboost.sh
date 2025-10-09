9f460afc7021fea3e48c33a22c977721284a2946
James Butler <jbbutler@login29.chn.perlmutter.nersc.gov>
2025-10-08 21:10:21 -0700

    adding project files

--- project/modelling/xgboost/cross_validation/rounds/snow_round1_xgboost.sh
+++ project/modelling/xgboost/cross_validation/rounds/snow_round1_xgboost.sh
@@ -8,7 +8,8 @@
 #SBATCH -o cv.out #File to which standard out will be written
 #SBATCH -e cv.err #File to which standard err will be written
 
-source ~/.bashrc
+cd ..
+module load conda
 conda activate antarctic_ars
 
-python cross_validation.py
+python cross_validation.py --x_cols max_ocean_SLP_gradient max_landfalling_v850hPa max_landfalling_omega500 max_IWV_ais cumulative_landfalling_area max_south_extent --y_col cumulative_snowfall_ais --hyperparam_json snow_round1_hyperparams.json --save_name snow_round1_cv_res.csv
