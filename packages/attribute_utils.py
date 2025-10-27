bug fixes in reanalysis masking script

packages/attribute_utils.py
@@ -11,16 +11,17 @@ October 2025
 import xarray as xr
 import pandas as pd
 import numpy as np
-from loading_utils import load_ais
+from loading_utils import *
+from st_dbscan import utils
 
-ais_mask = loading_utils.load_ais()
+ais_mask = load_ais()
 ais_mask = ais_mask.assign_coords(lat=ais_mask.lat.round(5), 
                                   lon=ais_mask.lon.round(5))
-cell_areas = loading_utils.load_cell_areas()
+cell_areas = load_cell_areas()
 cell_areas = cell_areas.assign_coords(lat=cell_areas.lat.round(5), 
                                       lon=cell_areas.lon.round(5)) # this is to avoid -0 not matching 0
 
-elevation = loading_utils.load_elevation()
+elevation = load_elevation()
 
 def is_landfalling(ar_da):
     '''
@@ -52,7 +53,7 @@ def compute_max_area(ar_da, ais_da=None):
     ar_da_rounded = ar_da.assign_coords(lat=ar_da.lat.round(5), lon=ar_da.lon.round(5))
     
     if ais_da is not None:
-        storm_ais_mask = ais_da.sel(lat=ar_da_rounded.lat, lon=ar_da_rounded.lon).Zwallybasins
+        storm_ais_mask = ais_da.sel(lat=ar_da_rounded.lat, lon=ar_da_rounded.lon)
         storm_da_subset = ar_da_rounded.where(storm_ais_mask, 0)
     else:
         storm_da_subset = ar_da_rounded.copy()
@@ -88,7 +89,7 @@ def compute_mean_area(ar_da, ais_da=None):
     ar_da_rounded = ar_da.assign_coords(lat=ar_da.lat.round(5), lon=ar_da.lon.round(5))
     
     if ais_da is not None:
-        storm_ais_mask = ais_da.sel(lat=ar_da_rounded.lat, lon=ar_da_rounded.lon).Zwallybasins
+        storm_ais_mask = ais_da.sel(lat=ar_da_rounded.lat, lon=ar_da_rounded.lon)
         storm_da_subset = ar_da_rounded.where(storm_ais_mask, 0)
     else:
         storm_da_subset = ar_da_rounded.copy()
@@ -112,7 +113,7 @@ def compute_cumulative_spacetime(ar_da, ais_da=None):
     ar_da_rounded = ar_da.assign_coords(lat=ar_da.lat.round(5), lon=ar_da.lon.round(5))
     
     if ais_da is not None:
-        storm_ais_mask = ais_da.sel(lat=ar_da_rounded.lat, lon=ar_da_rounded.lon).Zwallybasins
+        storm_ais_mask = ais_da.sel(lat=ar_da_rounded.lat, lon=ar_da_rounded.lon)
         storm_da_subset = ar_da_rounded.where(storm_ais_mask, 0)
     else:
         storm_da_subset = ar_da_rounded.copy()
@@ -254,7 +255,7 @@ def compute_cumulative(storm_da, var_da, area_da, ais_da=None):
         cumulative_storm_val (float): cumulative quantity underneath footprint of storm
     '''
     if ais_da is not None:
-        storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon).Zwallybasins
+        storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon)
         storm_da_subset = storm_da.where(storm_ais_mask, 0)
     else:
         storm_da_subset = storm_da.copy() # just so we can work with the name storm_da_subset later
@@ -281,7 +282,7 @@ def compute_max_intensity(storm_da, var_da, area_da, ais_da=None):
         max_intensity_val (float): the value of the maximum intensity
     '''
     if ais_da is not None:
-        storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon).Zwallybasins
+        storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon)
         storm_da_subset = storm_da.where(storm_ais_mask, 0)
     else:
         storm_da_subset = storm_da.copy() # just so we can work with the name storm_da_subset later
@@ -306,7 +307,7 @@ def compute_min_SLP(storm_da, var_da, area_da, ais_da):
     '''
 
     # grab the intersection of storm with ocean and AIS, respectively
-    storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon).Zwallybasins
+    storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon)
     storm_ocean_mask = np.logical_not(storm_ais_mask)
     storm_da_ais = storm_da.where(storm_ais_mask, 0)
     storm_da_ocean = storm_da.where(storm_ocean_mask, 0)
@@ -333,7 +334,7 @@ def compute_max_SLPgrad(storm_da, var_da, area_da, ais_da):
         max_grad (float): the max SLP gradient value
     '''
 
-    storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon).Zwallybasins
+    storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon)
     storm_ocean_mask = np.logical_not(storm_ais_mask)
     storm_da_ais = storm_da.where(storm_ais_mask, 0)
     storm_da_ocean = storm_da.where(storm_ocean_mask, 0)
@@ -381,7 +382,7 @@ def compute_avg_landfalling_minomega(storm_da, var_da, area_da, ais_da):
         omega_agg (float): the aggregate landfalling omega for that storm
     '''
     
-    storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon).Zwallybasins
+    storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon)
     storm_da_ais = storm_da.where(storm_ais_mask, 0)
     first_landfall = np.min(storm_da.time[storm_da_ais.any(dim=['lat', 'lon'])].values)
     storm_cell_areas = area_da.sel(lat=storm_da.lat, lon=storm_da.lon)
@@ -439,7 +440,7 @@ def compute_max_landfalling_wind(storm_da, var_da, area_da, ais_da):
         max_wind (float): the maximum 850 hPa wind over ocean at time of first landfall
     '''
 
-    storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon).Zwallybasins
+    storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon)
     storm_ocean_mask = np.logical_not(storm_ais_mask)
     storm_da_ais = storm_da.where(storm_ais_mask, 0)
     storm_da_ocean = storm_da.where(storm_ocean_mask, 0)
@@ -474,7 +475,7 @@ def compute_avg_landfalling_wind(storm_da, var_da, area_da, ais_da):
         avg_wind (float): the average 850 hPa wind over ocean at time of first landfall
     '''
 
-    storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon).Zwallybasins
+    storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon)
     storm_ocean_mask = np.logical_not(storm_ais_mask)
     storm_da_ais = storm_da.where(storm_ais_mask, 0)
     storm_da_ocean = storm_da.where(storm_ocean_mask, 0)
@@ -513,7 +514,7 @@ def compute_average(storm_da, var_da, area_da, ais_da=None):
     '''
     
     if ais_da is not None:
-        storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon).Zwallybasins
+        storm_ais_mask = ais_da.sel(lat=storm_da.lat, lon=storm_da.lon)
         storm_da_subset = storm_da.where(storm_ais_mask, 0)
     else:
         storm_da_subset = storm_da.copy()

packages/compute_attributes.py
@@ -100,7 +100,7 @@ def compute_anomaly_summaries(storm_da, func_vars_dict, climatology_dict, cell_a
         
     return summaries
 
-def compute_precip_summaries(storm_da, cell_areas, agg_func):
+def compute_precip_summaries(storm_da, cell_areas, agg_func, data_path):
     '''
     Function that computes summaries for precipitation variables. Precipitation requires
         separate treatment because even though the footprint of the AR may have left
@@ -123,7 +123,7 @@ def compute_precip_summaries(storm_da, cell_areas, agg_func):
     
     ds_lst = []
     for fname in fnames:
-        ds = xr.open_dataset(scratch_path + 'merra2_data_precip_ivt/' + fname)
+        ds = xr.open_dataset(data_path + fname)
         shifted = ds.assign_coords(time=ds.time - np.timedelta64(30, 'm'))
         upscaled = (shifted[var_lst]*60*60).resample(time='3h').sum() # don't remember why I did this...
         ds_lst.append(upscaled)

project/dataset/reanalysis_masking.py
@@ -91,8 +91,8 @@ summaries_lst_tavg1_precip = []
 for i in tqdm(range(landfalling_storms.shape[0])):
     
     storm = landfalling_storms.iloc[i].data_array
-    summaries_ivt = compute_raw_summaries(storm, func_vars_dict, cell_areas, ticker, tavg1_precip_data_path, ivt=True)
-    summaries_precip = compute_precip_summaries(storm, cell_areas, lambda storm_da, var_da, area_da: compute_cumulative(storm_da, var_da, area_da, ais_mask))
+    summaries_ivt = compute_raw_summaries(storm, func_vars_dict, cell_areas, ticker, tavg1_precip_data_path, half_hour=True)
+    summaries_precip = compute_precip_summaries(storm, cell_areas, lambda storm_da, var_da, area_da: compute_cumulative(storm_da, var_da, area_da, ais_mask), tavg1_precip_data_path)
     summaries = summaries_ivt + summaries_precip
     summaries_lst_tavg1_precip.append(summaries)
     
@@ -110,7 +110,7 @@ summaries_lst_tavg1_wind = []
 for i in tqdm(range(landfalling_storms.shape[0])):
     
     storm = landfalling_storms.iloc[i].data_array
-    summaries = compute_raw_summaries(storm, func_vars_dict, cell_areas, ticker, tavg1_850hPa_wind_data_path, ivt=True)
+    summaries = compute_raw_summaries(storm, func_vars_dict, cell_areas, ticker, tavg1_850hPa_wind_data_path, half_hour=True)
     summaries_lst_tavg1_wind.append(summaries)
     
 labels_tavg1_wind = np.array(list(func_vars_dict.keys()))[:,0]
@@ -151,7 +151,6 @@ region_defs_coarser = {'West': [-150, -30],
 
 region_masks_coarser = find_region_masks(region_defs_coarser, ais_mask)
 
-landfalling_storms['region'] = landfalling_storms['data_array'].apply(lambda x: find_landfalling_region(x, region_masks))
 landfalling_storms['coarser_region'] = landfalling_storms['data_array'].apply(lambda x: find_landfalling_region(x, region_masks_coarser))
 
 landfalling_storms['trajectory'] = landfalling_storms['data_array'].apply(extract_trajectory)
@@ -164,4 +163,4 @@ landfalling_storms[labels_inst3_omega] = summaries_lst_inst3_omega
 
 
 # save the dataframe
-landfalling_storms.to_hdf(proj_path/'dataset/datasets/landfalling_storm_quantities_df.h5', key='df')
+landfalling_storms.to_hdf(home_dir/'project/dataset/datasets/landfalling_storm_quantities_df.h5', key='df')