f85e7a411686aa702a519a68ae8c7adb1c24579a
James <butlerj@berkeley.edu>
2025-10-30 01:41:31 -0700

    further cv changes

--- project/modelling/xgboost/cross_validation/rounds/snow_round1_xgboost.sh
+++ project/modelling/xgboost/cross_validation/rounds/snow_round1_xgboost.sh
@@ -4,9 +4,9 @@
 #SBATCH --output snow_xgb_round1.out
 #SBATCH --error snow_xgb_round1.err
 #SBATCH -p high
-#SBATCH --cpus-per-task 10
+#SBATCH --cpus-per-task 15
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user butlerj@berkeley.edu
 export OMP_NUM_THREADS=1
 cd ..
-conda run -n antarctic_ars python cross_validation_xgb.py --x_cols max_ocean_SLP_gradient max_landfalling_v850hPa avg_landfalling_minomega max_IWV_ais cumulative_landfalling_area max_south_extent --y_col cumulative_snowfall_ais --hyperparam_json snow_round1_hyperparams.json --chunk_size 100  --save_name snow_xgb_round1.csv
+conda run -n antarctic_ars python cross_validation_xgb.py --x_cols max_ocean_SLP_gradient max_landfalling_v850hPa avg_landfalling_minomega max_IWV_ais cumulative_landfalling_area max_south_extent --y_col cumulative_snowfall_ais --hyperparam_json snow_round1_hyperparams.json --chunk_size 50  --save_name snow_xgb_round1.csv
