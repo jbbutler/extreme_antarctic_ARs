8dec415c6de7a87f08cb85516a97e54a1b773ac7
Jimmy Butler <butlerj@berkeley.edu>
2026-06-03 14:42:22 -0700

    adding results notebooks and python scripts

---
+++ notebooks/abbreviations_units.py
@@ -0,0 +1,15 @@
+# File with constant mappings of variable names to abbreviations and units
+
+ABBV_DICT = {'max_ocean_SLP_gradient': 'SLP',
+             'max_landfalling_v850hPa': 'Wind',
+             'avg_landfalling_minomega': 'Omega',
+             'max_IWV_ais': 'IWV',
+             'cumulative_landfalling_area': 'CLA',
+             'max_south_extent': 'South'}
+
+UNITS_DICT = {'max_ocean_SLP_gradient': 'hPa/km',
+              'max_landfalling_v850hPa': 'm/s',
+              'avg_landfalling_minomega': 'Pa/s',
+              'max_IWV_ais': 'kg/m$^{2}$',
+              'cumulative_landfalling_area': 'km$^{2}$ day',
+              'max_south_extent': 'deg. lat.'}
